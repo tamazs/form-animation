717e644dd0719e9c88b7dc2d996a6c3681f55eea
tamazs <majortamas313@gmail.com>
2022-04-09 15:49:33 +0200

    adding placeholder shift and validations

--- app.js
+++ app.js
@@ -15,7 +15,70 @@ containers.forEach(container => {
     input.addEventListener('focus', () => {
         if (!input.value){
             tl.fromTo(line, {attr: {d: start}}, {attr: {d: end}, ease: 'Power2.easeOut', duration: 0.75});
-            tl.to(line, {attr: {d: start}, ease: 'elastic.out(3, 0.5)'}, '<50%')
+            tl.to(line, {attr: {d: start}, ease: 'elastic.out(3, 0.5)'}, '<50%');
+            //Placeholder shift
+            tl.to(placeholder, {top: -15, left: 0, scale: 0.7, duration: 0.5, ease: "power2.easeOut"},'<15%');
         }
     })
 })
+
+//Revert back if its not focused
+form.addEventListener('click', () => {
+    containers.forEach(container => {
+        const input = container.querySelector('.input');
+        const line = container.querySelector('.elastic-line');
+        const placeholder = container.querySelector('.placeholder');
+
+        if(document.activeElement !== input){
+            if(!input.value){
+                gsap.to(placeholder, {top: 0, left: 0, scale: 1, duration: 0.5, ease: "Power2.easeOut"})
+            }
+        }
+        //Validation
+        //Name validation
+        input.addEventListener('input', (e) => {
+            if(e.target.type === 'text'){
+                let inputText = e.target.value;
+                if(inputText.length > 2){
+                    colorize('#6391E8', line, placeholder);
+                } else{
+                    colorize('#FE8C99', line, placeholder);
+                }
+            }
+        //Email validation
+        if(e.target.type === 'email'){
+            let valid = validateEmail(e.target.value);
+            if(valid){
+                colorize('#6391E8', line, placeholder);
+            } else{
+                colorize('#FE8C99', line, placeholder);
+            }
+        }
+        //Phone validation
+        if(e.target.type === 'tel'){
+            let valid = validatePhone(e.target.value);
+            if(valid){
+                colorize('#6391E8', line, placeholder);
+            } else{
+                colorize('#FE8C99', line, placeholder);
+            }
+        }
+        })
+    })
+})
+
+//Checking email validation
+function validateEmail(email) {
+    let re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+function validatePhone(phone) {
+    let re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+    return re.test(phone);
+}
+
+//Colorize function
+function colorize(color, line, placeholder){
+    gsap.to(line, {stroke: color, duration: 0.75});
+    gsap.to(placeholder, {color: color, duration: 0.75});
+}
