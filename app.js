f7e9fbed0612f6a1aad8615fa0273605a088719f
tamazs <majortamas313@gmail.com>
2022-04-09 16:30:28 +0200

    chracter animation

--- app.js
+++ app.js
@@ -101,3 +101,8 @@ checkbox.addEventListener('click', () => {
         tl2.to('.checkbox-label', {color: "#c5c5c5"}, '<')
     }
 })
+
+//Character animation
+gsap.set('#eye', {transformOrigin: 'center'});
+gsap.fromTo('#eye', {scaleY: 1}, {scaleY: 0.3, repeat: -1, yoyo: true, repeatDelay: 0.5, ease: 'Power2.easeOut'});
+gsap.fromTo('#eyebrow', {y: 0}, {y: -1, repeat: -1, yoyo: true, repeatDelay: 0.5, ease: 'Power2.easeOut'});
